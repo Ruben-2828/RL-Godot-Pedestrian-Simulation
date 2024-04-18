e289f01c22f4af59f94354de2275b48c05193843
LilQuacky <andreafalbo46@gmail.com>
2024-04-18 12:37:03 +0200

    feat: enable different area to place spawn and target

--- environment/level_P/random_spawn.gd
+++ environment/level_P/random_spawn.gd
@@ -1,44 +1,60 @@
-extends Area3D
+extends Node3D
 
 @onready var spawn = $Spawn
-@onready var area = $Area
+ 
+# list of areas to place the spawn
+var areas: Array = []
 
+# offset to position the spawner within the area
 var offset = 0.5
+
 @export_category("Spawn Settings")
+## randomize the position of the spawn inside the area
 @export var randomize_position = false
+## randomize the rotation of the spawn
 @export var randomize_rotation = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if randomize_position:
-		randomize_pos()
-	if randomize_rotation:
-		randomize_rot()
-
+	areas = find_children("CollisionShapeSpawn*")
+	set_random()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func randomize_pos():
+## perform randomization of spawn position
+func randomize_pos(area):
 	var shape = area.shape as BoxShape3D
 	var extents = shape.extents
-	
 	var random_pos = Vector3(
 		randf_range(-extents.x + offset, extents.x - offset),
 		0.0,
 		randf_range(-extents.z + offset, extents.z - offset),
 	)
-	spawn.transform.origin = random_pos
-	
+	spawn.position = random_pos
+
+## perform randomization of spawn rotation
 func randomize_rot():
 	var random_rot = randi_range(-4, 4) * 45
 	spawn.rotation_degrees = Vector3(0.0, random_rot, 0)
 
+## handle the randomization
+func set_random():
+	# randomize which area to place the spawner
+	var selected_area = areas[randi_range(0, areas.size() -1)]
+	spawn.global_position = selected_area.global_position
+	
+	# randomize position inside the selected area
+	if randomize_position:
+		randomize_pos(selected_area)
+		
+	# randomize rotation
+	if randomize_rotation:
+		randomize_rot()
+		
+## set the spawn to a new position when the agent reach the final target
 func _on_final_target_body_entered(body):
 	if body.is_class("CharacterBody3D"):
-		if randomize_position:
-			randomize_pos()
-		if randomize_rotation:
-			randomize_rot()
+		set_random()
 		
--- environment/level_P/random_target.gd
+++ environment/level_P/random_target.gd
@@ -1,24 +1,27 @@
-extends Area3D
+extends Node3D
 
 @onready var final_target = $FinalTarget
-@onready var area = $Area
+
+# list of areas to place the target
+var areas: Array = []
+
+# offset to place the target within the area
+var offset = 1.5
 
 @export_category("Target Settings")
 @export var randomize_position = false
 
-var offset = 1.5
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if randomize_position:
-		randomize_pos()
-
+	areas = find_children("CollisionShapeTarget*")
+	set_random_pos()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func randomize_pos():
+## perform randomization of target position
+func randomize_pos(area):
 	var shape = area.shape as BoxShape3D
 	var extents = shape.extents
 	var random_pos = Vector3(
@@ -26,9 +29,19 @@ func randomize_pos():
 		0.0,
 		randf_range(-extents.z + offset, extents.z - offset),
 	)
-	final_target.transform.origin = random_pos
+	final_target.position = random_pos
+	
+## handle the randomization
+func set_random_pos():
+	# randomize which area to place the target
+	var selected_area = areas[randi_range(0, areas.size() -1)]
+	final_target.global_position = selected_area.global_position
+	
+	#randomize position inside the selected area
+	if randomize_position:
+		randomize_pos(selected_area)
 
+## set the target to a new position when the agent reach the final target
 func _on_final_target_body_entered(body):
 	if body.is_class("CharacterBody3D"):
-		if randomize_position:
-			randomize_pos()
+		set_random_pos()
