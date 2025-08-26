a3c805126bcb84956c14625c3dc225df7060d28a
ruben <rubentenderini@gmail.com>
2025-08-26 12:12:11 +0200

    minor pt2

--- godot/utils/Sync.gd
+++ godot/utils/Sync.gd
@@ -434,8 +434,8 @@ func handle_message() -> bool:
 	var message = _get_dict_json_message()
 	if message["type"] == "close":
 		print("received close message, closing game")
-		#get_tree().quit()
-		#parent.finish()
+		get_tree().quit()
+		parent.finish()
 		get_tree().set_pause(false)
 		return true
 
@@ -501,8 +501,8 @@ func _reset_agents(agents = all_agents):
 	for agent in agents:
 		agent.reset()
 		# Aggiunta per multi agent
-		# if agent._player.disable:
-		# 	agent.done = true
+		if agent._player.disable:
+			agent.done = true
 
 
 func _get_obs_from_agents(agents: Array = all_agents):
